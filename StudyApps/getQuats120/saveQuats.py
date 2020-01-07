66b818c51eb74ae0262910d484a6c0554babee71
jiwankim <kjwan4435@gmail.com>
2020-01-07 21:26:28 +0900

    Jiwan_save_IMU

---
+++ StudyApps/getQuats120/saveQuats.py
@@ -0,0 +1,35 @@
+import serial
+import csv
+import os.path
+import timeit
+
+
+port = '/dev/cu.usbmodem14101'
+brate = 9600 #boudrate
+SubNumber=2
+
+check = 'start'
+arduino = serial.Serial(port, 9600)
+
+while True:
+    if arduino.readable():
+        res = arduino.readline()
+        dataline = res.decode()[:len(res) - 3].split(',')
+        start = timeit.default_timer()
+
+    if check == 'start':
+        if os.path.isfile("/Users/Jiwan/Documents/GitHub/HeadEyeTracking/StudyApps/getQuats120/sub1%d.csv"%SubNumber):
+            f = open('sub1%d.csv'%SubNumber, 'a')
+            wr = csv.writer(f, lineterminator='\n')
+            stop = timeit.default_timer()
+            wr.writerow(dataline + [stop-start])
+        else:
+            f = open('sub1%d.csv'%SubNumber, 'w')
+            wr = csv.writer(f, lineterminator='\n')
+            wr.writerow(["quatI","quatJ","quatK","quatReal","quatRadianAccuracy","IMUtimestamp"])
+            stop = timeit.default_timer()
+            wr.writerow(dataline + [stop-start])
+
+    #if 외부 통신장치 input이 있으면
+    #   f.close()
+    #   break
