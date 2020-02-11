8562b6631c9d2a27a95f659ee02b75e29758ee3f
yonghwan-shin <39160195+yonghwan-shin@users.noreply.github.com>
2020-02-11 17:35:48 +0900

    manipulating code

--- Analysis/SecondAnalysis/Analysis.py
+++ Analysis/SecondAnalysis/Analysis.py
@@ -9,26 +9,23 @@ blocks = range(5)
 
 
 def main():
-    [imu_file_list, eye_file_list, hololens_file_list] = get_one_subject_files(6)
-    current_info = [0, 'U', 'W', 2]
-    # imu_data = get_file_by_info(imu_file_list, current_info)
+    [imu_file_list, eye_file_list, hololens_file_list] = get_one_subject_files(999)
+    current_info = [1, 'U', 'W', 0]
+    imu_data = get_file_by_info(imu_file_list, current_info)
     eye_data = get_file_by_info(eye_file_list, current_info)
-    hololens_data = get_file_by_info(hololens_file_list, current_info)
+    # hololens_data = get_file_by_info(hololens_file_list, current_info)
 
     # df_imu = file_as_pandas(imu_data)
-    # manipulate_imu(df_imu)
-    df_eye = change_eye_dataframe(file_as_pandas(eye_data))
-    # eye_refined_data = []
-    # for row in df_eye.itertuples():
-    #     # print(row[2])
-    #     eye_refined_data.append(demjson.decode(row[2])['norm_pos'][1])
-    df_holo = file_as_pandas(hololens_data)
-
+    df_imu = manipulate_imu(file_as_pandas(imu_data))
+    df_eye = manipulate_eye(file_as_pandas(eye_data))
+    # plt.plot(df_imu['IMUtimestamp'],df_imu['rotationX'])
+    # df_holo = file_as_pandas(hololens_data)
+    plt.plot(df_eye['timestamp'], df_eye['phi'])
 
 
     # print(df_holo.columns)
     # plt.plot(df_holo['Timestamp'],df_holo['HeadRotationX'],'x')
-    plt.plot(df_holo['HeadRotationX'])
+    # plt.plot(df_holo['HeadRotationX'])
     # plt.plot(eye_refined_data)
 
     plt.show()
--- Analysis/SecondAnalysis/DataManipulation.py
+++ Analysis/SecondAnalysis/DataManipulation.py
@@ -11,7 +11,7 @@ def manipulate_imu(_imu_dataframe: pd.DataFrame):
     output = pd.DataFrame(data=angle_list, columns=['IMUtimestamp', 'rotationX', 'rotationY', 'rotationZ'])
     return output
 
-def change_eye_dataframe(_eye_dataframe: pd.DataFrame):
+def manipulate_eye(_eye_dataframe: pd.DataFrame):
     eye_list = []
     for row in _eye_dataframe.itertuples(index=False):
         python_timestamp = row[0]
@@ -19,15 +19,6 @@ def change_eye_dataframe(_eye_dataframe: pd.DataFrame):
         json_dict = demjson.decode(pupil_data)
         eye_list.append(json_dict)
     output = pd.DataFrame(eye_list)
+    output = output[output.confidence > 0.6]
     print(output)
-    # for row in _dataframe.itertuples():
-    #     python_timestamp = row[1]
-    #     pupil_row = row[2]
-    #     json_dict = demjson.decode(pupil_row)
-    #     print(json_dict['timestamp'], json_dict['norm_pos'], json_dict['phi'], json_dict['theta'])
-    #     output.append({'python_timestamp': python_timestamp, 'timestamp': json_dict['timestamp'],
-    #                    'norm_pos_x': json_dict['norm_pos'][0], 'norm_pos_y': json_dict['norm_pos'][1],
-    #                    'phi': json_dict['phi'], 'theta': json_dict['theta']}, ignore_index=True)
-    # _dataframe.drop(_dataframe.columns[2:], axis=1, inplace=True)
-    # _dataframe.columns = ['python_timestamp', 'pupil_data']
-    # return _dataframe
+    return output
