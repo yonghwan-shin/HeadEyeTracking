58dd0b4f7ef51d4f7e29c9ea4cbe5e4d918a2952
yonghwan-shin <39160195+yonghwan-shin@users.noreply.github.com>
2020-05-20 17:46:31 +0900

    update

--- Analysis/SecondAnalysis/new.py
+++ Analysis/SecondAnalysis/new.py
@@ -11,6 +11,7 @@ import statistics
 from pathlib import Path
 import pandas as pd
 import demjson
+import plotly.express as px
 
 # import statsmodels.api as sm
 
@@ -74,7 +75,7 @@ def data_prep(eye, holo, imu):
     eye['target_horizontal'] = target_horizontal
     eye['filtered_y'] = filtered_y
     eye['filtered_x'] = filtered_x
-    return eye, holo, imu, peaks, filtered_x,filtered_y,target_vertical,target_horizontal
+    return eye, holo, imu, peaks, filtered_x, filtered_y, target_vertical, target_horizontal
 
 
 def data_analysis(eye, holo, imu):
@@ -109,32 +110,72 @@ def data_analysis(eye, holo, imu):
     # fig, axs = plt.subplots(2, 1, figsize=[8, 8], sharex=False)
     if eye.shape[0] < 100: print(current_info, 'too short data');return
     filtered_y = Analysing_functions.butterworth_filter(eye.norm_y, fc=5)
-
+    filtered_x = Analysing_functions.butterworth_filter(eye.norm_x, fc=5)
     target_vertical = intp_holoX(eye.timestamp) + intp_holoThe(eye.timestamp)
+
+    target_horizontal = intp_holoY(eye.timestamp) - intp_holoPhi(eye.timestamp)
     filtered_target_vertical = Analysing_functions.butterworth_filter(target_vertical, fc=5)
     peaks, _ = signal.find_peaks(filtered_target_vertical, distance=30)
 
-    # axs[0].scatter(eye.timestamp, filtered_target_vertical, marker='+')
     peaks = np.append([0], peaks)
     peaks = np.append(peaks, [len(eye) - 1])
+    eye['target_vertical'] = target_vertical
+    eye['target_horizontal'] = target_horizontal
+    eye['filtered_y'] = filtered_y
+    eye['filtered_x'] = filtered_x
+    fig, axs = plt.subplots(2, 1, figsize=(15, 5),sharex=True)
+    axs[1].axhline(0)
+    axs[0].axhline(0)
+    ss = []
+
+
+    window_size = 40
+    estimation_size = 3
+    axs[0].plot(eye.timestamp,eye.target_vertical)
+    axs[0].axvline(eye.timestamp.iloc[0])
+    axs[0].axvline(eye.timestamp.iloc[window_size])
+    for i in range(eye.shape[0] - window_size - estimation_size):
+        y = eye.filtered_y[i:i + window_size]
+        t = eye.target_vertical[i:i + window_size]
+        timeline = eye.timestamp[i:i + window_size]
+        s, itc, r, p, std = stats.linregress(y, t)
+
+        estimation_timeline = eye.timestamp[i + window_size: i + window_size + estimation_size]
+        estimation_y = eye.filtered_y[i + window_size:i + window_size + estimation_size]
+        real_target=eye.target_vertical[i + window_size:i + window_size + estimation_size]
+        estimation_target = s * estimation_y + itc
+        # axs[0].scatter(timeline.iloc[-1], t.iloc[-1], marker='+', color='red')
+        # axs[0].scatter(timeline.iloc[-1], y.iloc[-1] * s + i, marker='+', color='blue', alpha=0.3)  # -t.iloc[-1]
+        axs[1].scatter(timeline.iloc[-1], s / 200, marker='+', color='black')
+        axs[0].scatter(estimation_timeline, estimation_target,marker='+', color = 'red',alpha=0.3)
+        axs[0].scatter(estimation_timeline,estimation_target-real_target,marker='x',color='black')
+        ss.append(s)
+        # meansloe = sum(ss) / (len(ss))
+
+    axs[1].axhline(sum(ss) / (len(ss) * 200))
+    plt.show()
+
     # for peak in peaks:
     # axs[0].axvline(eye.timestamp.iloc[peak])
-    for i in range(len(peaks) - 1):
-        slope_vertical, intercept_vertical, r_vertical, p_vertical, std_err_vertical = stats.linregress(
-            filtered_y[peaks[i]:peaks[i + 1]], filtered_target_vertical[peaks[i]:peaks[i + 1]])
-        if slope_vertical > 0: print(i, 'weired regression');continue
+    # for i in range(len(peaks) - 1):
+    #     slope_vertical, intercept_vertical, r_vertical, p_vertical, std_err_vertical = stats.linregress(
+    #         filtered_y[peaks[i]:peaks[i + 1]], filtered_target_vertical[peaks[i]:peaks[i + 1]])
+    #     if slope_vertical > 0: print(i, 'weired regression');continue
 
-        # axs[0].scatter(eye.timestamp.iloc[peaks[i]:peaks[i + 1]],
-        #                (filtered_y[peaks[i]:peaks[i + 1]] * slope_vertical + intercept_vertical), marker='+')
-        # axs[1].plot(filtered_y[peaks[i]:peaks[i + 1]],
-        #             filtered_y[peaks[i]:peaks[i + 1]] * slope_vertical + intercept_vertical)
-        # axs[1].scatter(filtered_y[peaks[i]:peaks[i + 1]], filtered_target_vertical[peaks[i]:peaks[i + 1]], marker='+',
-        #                alpha=0.3)
+    # axs[0].scatter(eye.timestamp.iloc[peaks[i]:peaks[i + 1]],
+    #                (filtered_y[peaks[i]:peaks[i + 1]] * slope_vertical + intercept_vertical), marker='+')
+    # axs[1].plot(filtered_y[peaks[i]:peaks[i + 1]],
+    #             filtered_y[peaks[i]:peaks[i + 1]] * slope_vertical + intercept_vertical)
+    # axs[1].scatter(filtered_y[peaks[i]:peaks[i + 1]], filtered_target_vertical[peaks[i]:peaks[i + 1]], marker='+',
+    #                alpha=0.3)
 
-        # sns.regplot(filtered_y,filtered_target_vertical,ax=axs[1],marker='+',scatter_kws={'color':'k','alpha':0.3})
-        linregress_results.append(slope_vertical)
-        print(i, slope_vertical, ':', r_vertical)
+    # sns.regplot(filtered_y,filtered_target_vertical,ax=axs[1],marker='+',scatter_kws={'color':'k','alpha':0.3})
 
+    # linregress_results.append(slope_vertical)
+    # print(i, slope_vertical, ':', r_vertical)
+    # fig = px.scatter(data_frame=eye, x='target_horizontal', y='target_vertical', color='confidence',
+    #                  animation_frame='timestamp')
+    # fig.show()
     print(current_info, 'drawn')
     # plt.show()
 
@@ -160,7 +201,7 @@ def one_trial(subject, target, env, pos, block):
 if __name__ == '__main__':
     subjects = [201]
     poss = ['W']
-    envs = ['W']
+    envs = ['U']
     linregress_results = []
     for subject in subjects:
         [imu_file_list, eye_file_list, hololens_file_list] = FileHandling.get_one_subject_files(subject, refined=True)
---
+++ Analysis/SecondAnalysis/plot_simulation.py
@@ -0,0 +1,2 @@
+
+import plotly.express as px
