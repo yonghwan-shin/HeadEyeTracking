93bc6d1a73dae3649cf147cc002af1d84410eedb
yonghwanshin <yonghwanshin@unist.ac.kr>
2020-12-16 13:21:53 +0900

    add data proccess pipeline

--- Analysis/HeadBehaviour/AnalysingFunctions.py
+++ Analysis/HeadBehaviour/AnalysingFunctions.py
@@ -4,7 +4,113 @@ import math
 import vg
 import matplotlib.pyplot as plt
 
+def winter_low(cutoff_freq, sample_time, x0, x1, x2, y1, y2, print_coeff=False):
+    """Filters a data sample based on two past unfiltered and filtered data samples.
 
+    2nd order low pass, single pass butterworth filter presented in Winter2009.
+
+    Parameters
+    ==========
+    cuttoff_freq: float
+        The desired lowpass cutoff frequency in Hertz.
+    sample_time: floaat
+        The difference in time between the current time and the previous time.
+    x0 : float
+        The current unfiltered signal, x_i
+    x1 : float
+        The unfiltered signal at the previous sampling time, x_i-1.
+    x2 : float
+        The unfiltered signal at the second previous sampling time, x_i-2.
+    y1 : float
+        The filtered signal at the previous sampling time, y_i-1.
+    y2 : float
+        The filtered signal at the second previous sampling time, y_i-2.
+
+    """
+    sampling_rate = 1 / sample_time  # Hertz
+
+    correction_factor = 1.0  # 1.0 for a single pass filter
+
+    corrected_cutoff_freq = np.tan(np.pi * cutoff_freq / sampling_rate) / correction_factor  # radians
+
+    K1 = np.sqrt(2) * corrected_cutoff_freq
+    K2 = corrected_cutoff_freq ** 2
+
+    a0 = K2 / (1 + K1 + K2)
+    a1 = 2 * a0
+    a2 = a0
+
+    K3 = a1 / K2
+
+    b1 = -a1 + K3
+    b2 = 1 - a1 - K3
+
+    if print_coeff:
+        print('num:', a0, a1, a2)
+        print('dem:', 1.0, -b1, -b2)
+
+    return a0 * x0 + a1 * x1 + a2 * x2 + b1 * y1 + b2 * y2
+
+def murphy_high(cutoff_freq, sample_time, x0, x1, x2, y1, y2, print_coeff=False):
+    """
+    Parameters
+    ==========
+    cuttoff_freq: float
+        The desired lowpass cutoff frequency in Hertz.
+    sample_time: floaat
+        The difference in time between the current time and the previous time.
+    x0 : float
+        The current unfiltered signal, x_i
+    x1 : float
+        The unfiltered signal at the previous sampling time, x_i-1.
+    x2 : float
+        The unfiltered signal at the second previous sampling time, x_i-2.
+    y1 : float
+        The filtered signal at the previous sampling time, y_i-1.
+    y2 : float
+        The filtered signal at the second previous sampling time, y_i-2.
+    """
+    sampling_rate = 1 / sample_time  # Hertz
+
+    correction_factor = 1.0
+
+    cutoff_freq = 1 / 2 / sample_time - cutoff_freq  # covert high pass freq to equivalent lowpass freq
+
+    corrected_cutoff_freq = np.tan(np.pi * cutoff_freq / sampling_rate) / correction_factor
+
+    K1 = np.sqrt(2) * corrected_cutoff_freq
+    K2 = corrected_cutoff_freq ** 2
+
+    a0 = K2 / (1 + K1 + K2)
+    a1 = 2 * a0
+    a2 = a0
+
+    K3 = a1 / K2
+
+    b1 = -a1 + K3
+    b2 = 1 - a1 - K3
+
+    c0 = a0
+    c1 = -a1
+    c2 = a2
+
+    d1 = -b1
+    d2 = b2
+
+    if print_coeff:
+        print('num:', c0, c1, c2)
+        print('dem:', 1.0, -d1, -d2)
+
+    return c0 * x0 + c1 * x1 + c2 * x2 + d1 * y1 + d2 * y2
+
+def realtime_lowpass(time, sig, cutoff):
+    output = [sig[0], sig[1]]
+    for i in range(2, len(time)):
+        output.append(winter_low(cutoff,
+                                 time[i] - time[i - 1],
+                                 sig[i], sig[i - 1], sig[i - 2],
+                                 output[i - 1], output[i - 2]))
+    return output
 def head_angle_to_angle_distance(H, V, tH, tV):
     H = H.apply(math.radians)
     tH = tH.apply(math.radians)
--- Analysis/HeadBehaviour/ThirdStudyPerformance.py
+++ Analysis/HeadBehaviour/ThirdStudyPerformance.py
@@ -2,9 +2,20 @@
 from AnalysingFunctions import *
 
 from FileHandling import *
+import time
+import plotly.graph_objects as go
+import plotly.io as pio
+import seaborn as sns
 
+sns.set_theme(style='whitegrid')
+pio.renderers.default = 'browser'
 dwell_thresholds = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
 rolling_size = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
+cutoff_freqs = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]
+
+
+def inverse(a):
+    return 1 / a
 
 
 def dwell_analysis(target, env, block, subject, output, apply: str):
@@ -15,14 +26,23 @@ def dwell_analysis(target, env, block, subject, output, apply: str):
         # continue;
 
     if env == 'W':
-        r = 0.03 * 5
+        r = 0.3 / 2
     else:
-        r = 0.03
-
+        r = 0.3 / 2
+    # output['MaximumTargetAngle'] = output.Distance.apply(inverse) * r
+    # output['MaximumTargetAngle'] = output['MaximumTargetAngle'].apply(math.asin)
     output['MaximumTargetAngle'] = (r * 1 / output.Distance).apply(math.asin) * 180 / math.pi
+    # output['MaximumTargetAngle'] =output['MaximumTargetAngle'].apply(math.degrees)
 
+    # plt.plot(output.timestamp,(r*1/output.Distance).apply(math.asin) * 180 / math.pi)
+    # plt.plot(output.timestamp,output.MaximumTargetAngle)
+    # plt.show()
+    # plt.plot(output.timestamp,output[apply])
+    # plt.show()
+    # if apply == 'default':
+    #     apply = 'angular_distance'
     initial_contact_time_data = output[output[apply] < output['MaximumTargetAngle']]
-    if len(initial_contact_time_data) <=0 :
+    if len(initial_contact_time_data) <= 0:
         return;
     initial_contact_time = initial_contact_time_data.timestamp.values[0]
 
@@ -32,14 +52,21 @@ def dwell_analysis(target, env, block, subject, output, apply: str):
     Maintain['targetOn'] = np.where(Maintain[apply] < Maintain['MaximumTargetAngle'], True, False)
     mean_error = Maintain.TargetHorizontal.mean()
     std_error = Maintain.TargetHorizontal.std()
+    max_angle_distance = Maintain[apply].max()
+
+    # plt.plot(Maintain.targetOn)
+    # plt.show()
+    # plt.plot(Maintain.target_entered)
+    # plt.show()
     # print('mean error:', mean_error, 'std error', std_error)
 
     dwell_list = []
     maintain_total_frame = Maintain.shape[0]
-    target_on_frame = (Maintain[Maintain.targetOn == True]).shape[0]
+    target_on_frame = len(Maintain[Maintain.targetOn == True])
 
     for k, l in itertools.groupby(Maintain.iterrows(), key=lambda row: row[1]['targetOn']):
-        dwell_list.append(pd.DataFrame([t[1] for t in l]))
+        if k == True:
+            dwell_list.append(pd.DataFrame([t[1] for t in l]))
 
     # print('total frame:', maintain_total_frame, 'target-on frame:', target_on_frame, 'target-on rate',
     #       target_on_frame / maintain_total_frame * 100, '%')
@@ -57,7 +84,7 @@ def dwell_analysis(target, env, block, subject, output, apply: str):
                   target=target, environment=env, block=block, subject=subject,
                   walklength=walklength,
                   initial_contact_time=initial_contact_time,
-                  mean_error=mean_error, std_error=std_error,
+                  mean_error=mean_error, std_error=std_error, max_angle_distance=max_angle_distance,
                   maintain_total_frame=maintain_total_frame, target_on_frame=target_on_frame,
                   target_on_rate=target_on_frame / maintain_total_frame * 100,
                   target_in_count=target_in_count, total_target_on_time=total_target_on_time,
@@ -85,6 +112,7 @@ def dwell_analysis(target, env, block, subject, output, apply: str):
         result['dwell_success_count_' + str(threshold)] = len(dwell_success_list)
         result['first_dwell_' + str(threshold)] = first_dwell_success_time
         result['mean_angular_speed_' + str(threshold)] = sum(mean_angular_speed) / len(dwell_success_list)
+
     return result
 
 
@@ -92,60 +120,150 @@ def dwell_analysis(target, env, block, subject, output, apply: str):
 
 
 # third
-subjects = range(1, 2)
+subjects = range(1, 17)
+# subjects = [1]
+# envs = ['W']
+# targets = [0]
+# blocks = [1]
 envs = ['U', 'W']
-targets=[0]
-blocks=[1]
-# targets = range(8)
-# blocks = range(1, 5)
+targets = range(8)
+blocks = range(1, 5)
 final_result = []
 
-for subject, env, target, block, roll in itertools.product(
-        subjects, envs, targets, blocks, rolling_size
+t = time.time()
+for subject, env, target, block in itertools.product(
+        subjects, envs, targets, blocks
 ):
+    for roll in rolling_size:
+        try:
+
+            print(subject, env, target, block, roll)
+
+            output = read_hololens_data(target=target, environment=env, posture='W', block=block, subject=subject,
+                                        study_num=3)
+            output['rolling_average' + str(roll)] = get_new_angular_distance(
+                output.head_rotation_y.rolling(roll, min_periods=1).mean(),
+                output.head_rotation_x.rolling(roll, min_periods=1).mean(), output)
+
+            result = dwell_analysis(target, env, block, subject, output, 'rolling_average' + str(roll))
+
+            if result is not None:
+                final_result.append(result)
+        except Exception as e:
+            print(e)
+    for cutoff in cutoff_freqs:
+        try:
+
+            print(subject, env, target, block, cutoff)
+
+            output = read_hololens_data(target=target, environment=env, posture='W', block=block, subject=subject,
+                                        study_num=3)
+            output['lowpass' + str(cutoff)] = get_new_angular_distance(
+                pd.Series(realtime_lowpass(output.timestamp,output.head_rotation_y,cutoff)),
+                pd.Series(realtime_lowpass(output.timestamp,output.head_rotation_x,cutoff)), output)
+
+            result = dwell_analysis(target, env, block, subject, output, 'lowpass' + str(roll))
+
+            if result is not None:
+                final_result.append(result)
+        except Exception as e:
+            print(e)
     try:
-        print(subject, env, target, block, roll)
         output = read_hololens_data(target=target, environment=env, posture='W', block=block, subject=subject,
                                     study_num=3)
-        output['rolling_average'+str(roll)] = get_new_angular_distance(output.head_rotation_y.rolling(roll, min_periods=1).mean(),
-                                                     output.head_rotation_x.rolling(roll, min_periods=1).mean(), output)
-
-        result = dwell_analysis(target, env, block, subject, output, 'rolling_average'+str(roll))
+        output['default'] = get_new_angular_distance(
+            output.head_rotation_y, output.head_rotation_x, output
+        )
+        result = dwell_analysis(target, env, block, subject, output, 'default')
 
         if result is not None:
             final_result.append(result)
     except Exception as e:
         print(e)
 summary = pd.DataFrame(final_result)
-summary.to_csv('summary3.csv')
+summary.to_csv('summary3_LP.csv')
+print('overall time', time.time() - t)
 # %%
+from natsort import natsorted
+
 summary = pd.read_csv('summary3.csv')
+
 UI = summary[summary.environment == 'U']
 World = summary[summary.environment == 'W']
-rolling_size = [30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
-dwell_thresholds = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
-plot_dwell_columns = ['dwell_success_frame', 'dwell_success_count', 'first_dwell', 'mean_angular_speed']
-for col in plot_dwell_columns:
-    for roll in rolling_size:
-        UI1 = UI[UI['apply'] == roll].fillna(0)
-        plt.plot(dwell_thresholds, [UI1[col + "_" + str(x)].mean() for x in dwell_thresholds], label=str(roll))
-    plt.title(str(col))
-    plt.legend()
-    plt.show()
-# plt.show()
-# for columnName in plot_dwell_columns:
-#     draw_walk_plot(UI, World, columnName, dwell_thresholds)
+comparison = ['default'] + ['rolling_average' + str(roll) for roll in rolling_size]
 
+mean_dataframe = summary.groupby(['environment', 'apply']).mean()
+mean_dataframe = mean_dataframe.reindex(natsorted(mean_dataframe.index))
 
-# rolling_size=[30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
-# plot_rolling_columns = ['H_rolling_initial_contact_time', 'H_rolling_mean_error', 'H_rolling_std_error',
-#                         'V_rolling_initial_contact_time', 'V_rolling_mean_error', 'V_rolling_std_error']
-# for columnName in plot_rolling_columns:
-#     # draw_rolling_plot(columnName)
-#     draw_walk_plot(UI,World, columnName, rolling_size)
+dwell_success_frame_columns = [col for col in mean_dataframe.columns if 'dwell_success_frame' in col]
+dwell_success_count_columns = [col for col in mean_dataframe.columns if 'dwell_success_count' in col]
+
+first_dwell_columns = [col for col in mean_dataframe.columns if 'first_dwell' in col]
+# UI_dwell_success_rate = 100 - UI.isnull().groupby('apply').sum().astype(int) / len(UI)*100
+UI_dwell_success_rate = UI.groupby('apply').apply(lambda x: x.notnull().mean())
+UI_dwell_success_rate = UI_dwell_success_rate.reindex(natsorted(UI_dwell_success_rate.index))
+UI_dwell_success_rate[dwell_success_frame_columns].plot();
+plt.show()
+mean_dataframe.loc['U'][dwell_success_count_columns].plot();
+plt.show()
+mean_dataframe.loc['U'][dwell_success_frame_columns].plot();
+plt.show()
+mean_dataframe.loc['U'][first_dwell_columns].plot();
+plt.show()
+
 #
-# lowpass_cutoff = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# plot_lowpass_columns = ['H_lowpass_initial_contact_time', 'H_lowpass_std_error',
-#                         'V_lowpass_initial_contact_time', 'V_lowpass_std_error']
-# for columnName in plot_lowpass_columns:
-#     draw_walk_plot(UI,World,columnName, lowpass_cutoff)
+fig_basic, (ax_init_time, ax_max_angle_distance, ax_rate, ax_in_count) = plt.subplots(4, 1, figsize=(10, 20),
+                                                                                      sharex=True)
+fig_dwell, (ax_dwell_success_count, ax_first_dwell, ax_dwell_success_rate) = plt.subplots(3, 1, figsize=(10, 15),
+                                                                                          sharex=True)
+
+for subset in comparison:
+    # data = World[World['apply'] == subset]
+    data = UI[UI['apply'] == subset]
+    print(subset, len(data), end=':');
+    # print('init', data['initial_contact_time'].mean(), '\t',
+    #       'max', data['max_angle_distance'].mean(), '\t',
+    #       'rate', data['target_on_rate'].mean(), '\t',
+    #       'in count', data['target_in_count'].mean(), '\t',
+    #       'total time', data['total_target_on_time'].mean(), '\t',
+    #       'mean time', data['mean_target_on_time'].mean(), '\t', )
+
+    ax_init_time.bar(subset, data['initial_contact_time'].mean(), color=['red'])
+    ax_max_angle_distance.bar(subset, data['max_angle_distance'].mean())
+    ax_rate.bar(subset, data['target_on_rate'].mean())
+
+    ax_in_count.bar(subset, data['target_in_count'].mean())
+
+    dwell_success_counts = []
+    dwell_first_dwells = []
+    dwell_success_rates = []
+    fail_count = data.isnull().sum(axis=0)
+    for dwell_th in dwell_thresholds:
+        total_trial = len(data['dwell_success_count_' + str(dwell_th)])
+
+        dwell_success_rates.append(
+            (total_trial - fail_count['dwell_success_count_' + str(dwell_th)]) / total_trial * 100
+        )
+        dwell_success_counts.append(data['dwell_success_count_' + str(dwell_th)].mean())
+        dwell_first_dwells.append(data['first_dwell_' + str(dwell_th)].mean())
+    ax_dwell_success_count.plot(dwell_thresholds, dwell_success_counts, label=subset)
+    ax_first_dwell.plot(dwell_thresholds, dwell_first_dwells)
+    ax_dwell_success_rate.plot(dwell_thresholds, dwell_success_rates)
+ax_init_time.set_ylabel('initial contact time (s)')
+ax_init_time.title.set_text('initial contact time (s)')
+ax_max_angle_distance.set_ylabel('maximum angle distance (degree)')
+ax_max_angle_distance.title.set_text('maximum angle distance (degree)')
+ax_rate.set_ylabel('target-on rate (%)')
+ax_rate.title.set_text('target-on rate (%)')
+ax_in_count.set_ylabel('target-in count')
+ax_in_count.title.set_text('target-in count')
+ax_dwell_success_count.set_ylabel('dwell success count')
+ax_dwell_success_count.title.set_text('dwell success count')
+ax_first_dwell.set_ylabel('first dwell time (s)')
+ax_first_dwell.title.set_text('first dwell time (s)')
+ax_dwell_success_rate.set_ylabel('dwell success rate (%)')
+ax_dwell_success_rate.title.set_text('dwell success rate (%)')
+ax_dwell_success_rate.grid()
+fig_basic.show()
+fig_dwell.legend()
+fig_dwell.show()
